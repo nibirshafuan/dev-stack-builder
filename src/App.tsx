8872c4a7bdae0a247ecc9a2a44468ce555bafd1c
nibirshafuan <shafuannibir124@gmail.com>
2026-09-12 17:26:47 +0600

    chore: setup project structure

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <>
+      <Nav />
+
+      <section id="home" className="h-screen">
+      </section>
+
+      <section id="technologies" className="h-screen">
+      </section>
+
+      <section id="projects" className="h-screen">
+      </section>
+
+      <section id="about" className="h-screen">
+      </section>
+
+      <section id="contact" className="h-screen">
+      </section>
+    </>
+  );
+}
+
+export default App;
